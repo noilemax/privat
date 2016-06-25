c26eee6f4de91d9e0017c913da0d14429540db22
noilemax <noilemax@gmail.com>
2016-06-25 15:11:47 +0300

    добавление/удаление в вирт. базу

--- src/main/java/taskivanitsyarest/Application.java
+++ src/main/java/taskivanitsyarest/Application.java
@@ -13,9 +13,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import taskivanitsyarest.dao.entity.User;
 import taskivanitsyarest.dao.repository.UserRepository;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
@@ -33,20 +30,44 @@ public class Application {
     public CommandLineRunner createDefaultData(UserRepository userRepository) {
         return (args) -> {
 
-            userRepository.save(new User("Vasya", "qwerty123", (byte) 1));
-            userRepository.save(new User("Some Guy", "qwerty123", (byte) 1));
-            userRepository.save(new User("Ivan", "qwerty123", (byte) 1));
-            userRepository.save(new User("Vasya4", "qwerty123", (byte) 1));
-            userRepository.save(new User("Qwerty", "qwerty123", (byte) 1));
+            userRepository.save(new User("Vasya", "qwe23", (byte) 1));
+            userRepository.save(new User("Some Guy", "123", (byte) 1));
+            userRepository.save(new User("Ivan", "qwedfg23", (byte) 1));
+            userRepository.save(new User("Vasya4", "qwerty321", (byte) 1));
+            userRepository.save(new User("Qwerty", "qbnmfgy3", (byte) 1));
 
-
-            log.info("LunchMenu findAll():");
-            log.info("-------------------------------");
+            log.info("\n");
+            log.info("--findAll-----------------------------");
             for (User user1 : userRepository.findAll()) {
-                log.info(user1.toString());
+                log.info("ID: "+user1.getId().toString() + "  USERNAME: "+user1.getUsername().toString() + "  USER_PASSSWORD: " + user1.getPassword().toString());
             }
             log.info("-------------------------------");
 
+            log.info("\n");
+            log.info("--findOne (2user)-----------------------------");
+            log.info("ID: " + userRepository.findOne(2L).getId().toString()
+                    + "  NAME: " + userRepository.findOne(2L).getUsername().toString()
+                    + "  PASSWORD: " + userRepository.findOne(2L).getPassword().toString()
+                    + "  ISaCTIV: " + userRepository.findOne(2L).getIsactive().toString());
+            log.info("-------------------------------");
+
+            userRepository.delete(3L);
+
+            log.info("\n");
+            log.info("--deleteOne (3user)-----------------------------");
+            for (User user1 : userRepository.findAll()) {
+                log.info("ID: "+user1.getId().toString() + "  USERNAME: "+user1.getUsername().toString() + "  USER_PASSSWORD: " + user1.getPassword().toString());
+            }
+            log.info("-------------------------------");
+
+            userRepository.save(new User("New User", "newpass", (byte) 1));
+
+            log.info("\n");
+            log.info("--findAll-----------------------------");
+            for (User user1 : userRepository.findAll()) {
+                log.info("ID: "+user1.getId().toString() + "  USERNAME: "+user1.getUsername().toString() + "  USER_PASSSWORD: " + user1.getPassword().toString());
+            }
+            log.info("-------------------------------");
         };
     }
 }
